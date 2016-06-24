7f7bcdbd3acf70a97c2732eed4c89b4214cd65c2
dinagrinboim <dinigrinboim@gmail.com>
2016-06-24 16:43:07 +0300

    no message

---
+++ Pickage/app/src/main/java/app/pickage/com/pickage/UserActivities/FillPackageDetails.java
@@ -0,0 +1,15 @@
+package app.pickage.com.pickage.UserActivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.pickage.com.pickage.R;
+
+public class FillPackageDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fill_package_details);
+    }
+}
---
+++ Pickage/app/src/main/java/app/pickage/com/pickage/UserActivities/PindingMessenger.java
@@ -0,0 +1,15 @@
+package app.pickage.com.pickage.UserActivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.pickage.com.pickage.R;
+
+public class PindingMessenger extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pinding_messenger);
+    }
+}

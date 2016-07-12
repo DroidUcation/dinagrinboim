6dff77c405486f877157ec1069e026ee93852632
dinagrinboim <dinigrinboim@gmail.com>
2016-07-12 21:11:00 +0300

    Messenger main activity

---
+++ Pickage/app/src/main/java/app/pickage/com/pickage/MessengerActivities/MessengerMainActivity.java
@@ -0,0 +1,14 @@
+package app.pickage.com.pickage.MessengerActivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import app.pickage.com.pickage.R;
+
+public class MessengerMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messenger_main);
+    }
+}
